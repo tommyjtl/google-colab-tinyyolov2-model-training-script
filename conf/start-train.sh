2954b99c3ecde83cfae262173e5f4257ab8c8801
Tommy <i@tjtl.io>
2020-03-18 15:50:24 +0800

    update

--- conf/start-train.sh
+++
@@ -1,1 +0,0 @@
-../tools/darknet-colab/darknet detector train trash.data trash.cfg darknet19_448.conv.23
--- conf/test-train.sh
+++
@@ -1,1 +0,0 @@
-../tools/darknet-colab/darknet detector test trash.data trash.cfg ../backup/trash_last.weights ../test/trash_test.jpg
