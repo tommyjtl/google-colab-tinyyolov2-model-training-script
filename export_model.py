c4249e3c1cfa1c870dd1c727f7549d7a8fd4e9eb
Tommy <i@tjtl.io>
2020-03-18 21:56:52 +0800

    update

--- export_model.py
+++ export_model.py
@@ -26,10 +26,11 @@ def copy_raw_to_darkflow():
 def run_darkflow_convertion():
 	batcmd="bash check-aws.sh"
 	result = subprocess.check_output(batcmd, shell=True)
-	if result.decode('unicode_escape') == "yes":
+	# print(result.decode('unicode_escape').strip("\n"))
+	if result.decode('unicode_escape').strip("\n") == "yes":
 		print("this is an aws server, activating tensorflow env...")
-		os.system("* source activate tensorflow_p36")
-	elif result.decode('unicode_escape') != "yes":
+		os.system("source activate tensorflow_p36")
+	elif result.decode('unicode_escape').strip("\n") == "no":
 		print("this is not an aws server, just go with the flow...")
 
 	os.chdir("./tools/darkflow-colab/")
