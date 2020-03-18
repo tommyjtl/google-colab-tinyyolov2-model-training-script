c27c3cefd0537ce4ea659824d7894866a375e121
Tommy <i@tjtl.io>
2020-03-18 09:11:24 +0800

    update

---
+++ clear_conf.py
@@ -0,0 +1,65 @@
+import glob, random, os, time, shutil
+
+names_file_name = ""
+file_exist_state = False
+
+def delete_conf():
+	try:
+		os.chdir("./conf/")
+		os.system("rm start-train.sh")
+		os.system("rm ./*.cfg")
+		os.system("rm ./*.data")
+		os.system("rm ./*.names")
+	except IOError as e:
+		print(str(e))
+	finally:
+		os.chdir("../")
+
+def delete_data():
+	try:
+		os.chdir("./data/")
+		os.remove("test.txt")
+		os.remove("train.txt")
+		names_file_name = ""
+		for names_file in glob.glob("*.names"):
+			names_file_name = names_file
+			print(names_file_name)
+			os.remove(names_file_name)
+		# if os.name == "posix": os.rmdir(".DS_Store")
+		data_file_list = os.listdir("./")
+		data_file_list.remove("generate_train-test.py")
+		data_file_list.remove(".DS_Store")
+		for i in range(len(data_file_list)): 
+			print("Deleting " + data_file_list[i] + "...")
+			shutil.rmtree(data_file_list[i])
+	except IOError as e:
+		print(str(e))
+	finally:
+		os.chdir("../")
+
+try:
+	os.chdir("./conf/")
+	for names_file in glob.glob("*.data"):
+		names_file_name = names_file
+		# print(names_file_name)
+	if names_file_name == "":
+		print("conf files don't exist") 
+		file_exist_state = False
+	elif names_file_name != "":
+		print("conf files is not empty") 
+		file_exist_state = True
+except IOError as e:
+	print(str(e))
+finally:
+	os.chdir("../")
+
+if file_exist_state == False:
+	print("conf doesn't exist")
+elif file_exist_state == True:
+	check_if_continue = input("conf exists, are you sure to delete them? (y/n): ")
+	if check_if_continue == "y":
+		print("deleting all...\n")
+		delete_conf()
+		delete_data()
+	elif check_if_continue == "n":
+		print("does nothing, exiting now.")
--- generate_conf.py
+++ generate_conf.py
@@ -1,7 +1,8 @@
 import glob, random, os, time
 
 conf_directory_name = "conf"
-google_drive_path = "/home/ubuntu/projects/"
+# google_drive_path = "/home/ubuntu/projects/"
+google_drive_path = "/Users/tommyjtl/Documents/CocoRobo/"
 
 google_drive_project_path = input("Please enter the dataset directory path you put on Google Drive: \n") 
 classes_count = input("Please enter the number of classes you would like to train: \n")
@@ -9,7 +10,7 @@ project_name = input("Please enter the name of your project: \n")
 current_directory_path = os.getcwd()
 
 def generate_cfg():
-	print("1. Total classes is " + classes_count + ", " + project_name + ".cfg generated to conf directory")
+	print("\n1. Total classes is " + classes_count + ", " + project_name + ".cfg generated to conf directory")
 	f = open(conf_directory_name+ "/" + project_name + ".cfg", "w")
 	cfg_content = ["[net]\n", "Training\n", "batch=64\n", "subdivisions=16\n", "width=224\n", "height=224\n", "channels=3\n", "momentum=0.9\n", "decay=0.0005\n", "angle=0\n", "saturation = 1.5\n", "exposure = 1.5\n", "hue=.1\n\n", "learning_rate=0.001\n", "burn_in=1000\n", "max_batches=" + str(int(float(classes_count))*2000) +"\n", "policy=steps\n", "steps=" + str(int(float(classes_count))*0.8) + "," + str(int(float(classes_count))*0.9) + "\n", "scales=.1,.1\n\n", "[convolutional]\n", "batch_normalize=1\n", "filters=16\n", "size=3\n", "stride=1\n", "pad=1\n", "activation=leaky\n\n", "[maxpool]\n", "size=2\n", "stride=2\n\n", "[convolutional]\n", "batch_normalize=1\n", "filters=32\n", "size=3\n", "stride=1\n", "pad=1\n", "activation=leaky\n\n", "[maxpool]\n", "size=2\n", "stride=2\n\n", "[convolutional]\n", "batch_normalize=1\n", "filters=64\n", "size=3\n", "stride=1\n", "pad=1\n", "activation=leaky\n\n", "[maxpool]\n", "size=2\n", "stride=2\n\n", "[convolutional]\n", "batch_normalize=1\n", "filters=128\n", "size=3\n", "stride=1\n", "pad=1\n", "activation=leaky\n\n", "[maxpool]\n", "size=2\n", "stride=2\n\n", "[convolutional]\n", "batch_normalize=1\n", "filters=256\n", "size=3\n", "stride=1\n", "pad=1\n", "activation=leaky\n\n", "[maxpool]\n", "size=2\n", "stride=2\n\n", "[convolutional]\n", "batch_normalize=1\n", "filters=512\n", "size=3\n", "stride=1\n", "pad=1\n", "activation=leaky\n\n", "[convolutional]\n", "size=1\n", "stride=1\n", "pad=1\n", "filters=" + str((int(float(classes_count)) + 5) * 5) + "\n", "activation=linear\n\n", "[region]\n", "anchors = 0.57273, 0.677385, 1.87446, 2.06253, 3.33843, 5.47434, 7.88282, 3.52778, 9.77052, 9.16828\n", "bias_match=1\n", "classes=" + str(int(float(classes_count))) + "\n", "coords=4\n", "num=5\n", "softmax=1\n", "jitter=.2\n", "rescore=0\n\n", "object_scale=5\n", "noobject_scale=1\n", "class_scale=1\n", "coord_scale=1\n\n", "absolute=1\n", "thresh = .6\n", "random=1"]
 	f.writelines(cfg_content)
@@ -20,10 +21,10 @@ def generate_data():
 	f = open(conf_directory_name+ "/" + project_name + ".data", "w")
 	data_content = [
 		"classes="+ str(int(float(classes_count))) +"\n",
-		"train  = "+current_directory_path+"/data/train.txt\n",
-		"valid  = "+current_directory_path+"/data/test.txt\n",
+		"train  = " + current_directory_path + "/data/train.txt\n",
+		"valid  = " + current_directory_path + "/data/test.txt\n",
 		"names = " + project_name + ".names\n",
-		"backup = "+current_directory_path+"/backup\n"
+		"backup = " + current_directory_path + "/backup\n"
 	]
 	f.writelines(data_content)
 	f.close()
@@ -33,7 +34,7 @@ def generate_names():
 	names_file_name = ""
 	for names_file in glob.glob("*.names"):
 		names_file_name = names_file
-		print(names_file_name)
+		# print(names_file_name)
 	f = open(names_file_name,"r+")
 	read_original_names = f.readlines()
 	print(read_original_names)
