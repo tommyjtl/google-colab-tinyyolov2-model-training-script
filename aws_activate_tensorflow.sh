e17914cf6bd5bfd38c208106e1c43e0f66acdcb6
Tommy <i@tjtl.io>
2020-03-18 22:17:13 +0800

    update

--- aws_activate_tensorflow.sh
+++ aws_activate_tensorflow.sh
@@ -2,8 +2,7 @@ echo "First arg: $1"
 
 if [ $1 == 'enter' ]
 then
-	source activate tensorflow_p36
-elif [ $1 == 'exit' ]
-then 
-	source deactivate
+source activate tensorflow_p36
+else
+source deactivate
 fi
