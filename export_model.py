a79d30dc09bfcdaa43a70f80b0cc26dd8c47c7d7
Tommy <i@tjtl.io>
2020-03-18 22:29:11 +0800

    update

--- export_model.py
+++ export_model.py
@@ -29,7 +29,6 @@ def run_darkflow_convertion():
 	# print(result.decode('unicode_escape').strip("\n"))
 	if result.decode('unicode_escape').strip("\n") == "yes":
 		print("this is an aws server, activating tensorflow env...")
-		os.system("conda activate tensorflow_p36")
 		# exec_tensorflow_env = subprocess.check_output("source activate tensorflow_p36", shell=False)
 		# print(exec_tensorflow_env.decode('unicode_escape').strip("\n"))
 	elif result.decode('unicode_escape').strip("\n") == "no":
