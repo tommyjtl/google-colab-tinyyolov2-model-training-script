e8681acc13b5de36f3c5c5a67cd00b447f958e10
Tommy <i@tjtl.io>
2020-03-18 21:56:11 +0800

    update

--- get_output.py
+++ get_output.py
@@ -4,8 +4,8 @@ batcmd="bash check-aws.sh"
 result = subprocess.check_output(batcmd, shell=True)
 print(result.decode('unicode_escape').strip("\n"))
 
-if result.decode('unicode_escape') == "yes":
+if result.decode('unicode_escape').strip("\n") == "yes":
 	print("this is an aws server, activating tensorflow env...")
 	os.system("source activate tensorflow_p36")
-elif result.decode('unicode_escape') == "no":
+elif result.decode('unicode_escape').strip("\n") == "no":
 	print("this is not an aws server, just go with the flow...")
