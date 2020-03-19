f3423985f54e13997f7618c517ea7f1d5e5ff82d
Tommy <i@tjtl.io>
2020-03-19 17:00:19 +0800

    update

--- export_model.py
+++ export_model.py
@@ -97,9 +97,9 @@ def exec_final_convertion():
 
 if __name__ == '__main__':
 	generate_test_images_for_conversion()
-	# clear_darflow_label()
-	# copy_raw_to_darkflow()
-	# run_darkflow_convertion()
+	clear_darflow_label()
+	copy_raw_to_darkflow()
+	run_darkflow_convertion()
 	copy_to_tfliteconversion()
-	# prepare_ncc_package()
+	prepare_ncc_package()
 	exec_final_convertion()
