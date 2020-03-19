4e409ce147410c3e98b7cde07624c62c8fecd090
Tommy <i@tjtl.io>
2020-03-19 20:37:40 +0800

    update

---
+++ conf/test-train.sh
@@ -0,0 +1,3 @@
+cd /Users/tommyjtl/Documents/CocoRobo/google-colab-template/google-colab-tinyyolov2-model-training-script/tools/darknet-colab
+./darknet detector test ../../conf/trash.data ../../conf/trash.cfg ../../backup/trash_last.weights ../../test/trash_test.jpg
+cd /Users/tommyjtl/Documents/CocoRobo/google-colab-template/google-colab-tinyyolov2-model-training-script
