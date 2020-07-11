1d15184778a6d25189852bb553848186d319ad38
Tommy <i@tjtl.io>
2020-07-11 18:42:07 +0800

    update

--- generate_conf.py
+++ generate_conf.py
@@ -2,8 +2,8 @@ import glob, random, os, time
 
 conf_directory_name = "conf"
 # google_drive_path = "/home/ec2-user/SageMaker/"
-# google_drive_path = "/home/ubuntu/projects/"
-google_drive_path = "/content/"
+google_drive_path = "/home/ubuntu/projects/"
+# google_drive_path = "/content/"
 # google_drive_path = "/Users/tommyjtl/Documents/CocoRobo/"
 
 google_drive_project_path = input("Please enter the name of your project inside Google Drive: \n") 
@@ -80,6 +80,50 @@ def generate_testing_bash():
 	f.close()
 	os.chdir(current_directory_path)
 
+def generate_subprocess_check_py():
+	f = open(conf_directory_name+ "/train.py", "w")
+	train_check_content = [
+		"import subprocess, shlex, os, signal\n",
+		"\n",
+		"def run_command(command):\n",
+		"	process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)\n",
+		"	print(\"Process PID is: \" + str(process.pid))\n",
+		"	while True:\n",
+		"		output = process.stdout.readline()\n",
+		"		if output == '' and process.poll() is not None:\n",
+		"			break\n",
+		"		if output:\n",
+		"			formatted_output = output.strip().decode(\"utf-8\")\n",
+		"			if (\"avg loss\" in formatted_output) and (\"rate\" in formatted_output):\n",
+		"				print(formatted_output)\n",
+		"				iteration_times = int(formatted_output.split()[0][:-1])\n",
+		"				avg_loss = float(formatted_output.split()[2])\n",
+		"				# print(str(int(iteration_times)) + \",\" + str(float(formatted_output.split()[2])))\n",
+		"				if avg_loss < 0.06: break\n",
+		"	# process.terminate()\n",
+		"	process.terminate()\n",
+		"	try:\n",
+		"		process.wait(timeout=0.2)\n",
+		"		print('== subprocess exited with rc =', process.returncode)\n",
+		"	except subprocess.TimeoutExpired:\n",
+		"		print('subprocess did not terminate in time')\n",
+		"	os.kill(int(process.pid)+1, signal.SIGKILL)\n",
+		"\n",
+		"try:\n",
+		"	run_command(\"bash start-train.sh\")\n",
+		"except KeyboardInterrupt:\n",
+		"	print(\"Keyboard Interrupted.\")\n",
+		"\n",
+		"print(\"\"\"\n",
+		"  ____                   _ \n",
+		" |  _ \\  ___  _ __   ___| |\n",
+		" | | | |/ _ \\| '_ \\ / _ \\ |\n",
+		" | |_| | (_) | | | |  __/_|\n",
+		" |____/ \\___/|_| |_|\\___(_)\n",
+		"	\"\"\")\n"
+	]
+	f.writelines(train_check_content)
+	f.close()
 
 if __name__ == '__main__':
 	# shenzhen_trash_classification_sign_dataset
@@ -92,3 +136,4 @@ if __name__ == '__main__':
 	exec(open("generate_train-test.py").read())
 	os.chdir(current_directory_path)
 	generate_testing_bash()
+	generate_subprocess_check_py()
